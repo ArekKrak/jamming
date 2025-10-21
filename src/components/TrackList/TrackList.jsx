44c3330e0b9a24b00ede62dfcac21a473b0658db
Arek Krakowiak <369arek12@protonmail.com>
2025-10-21 20:11:06 +0100

    add method to TrackList

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,7 +1,12 @@
+import Track from "../Track/Track"
+
 export default function TrackList({ tracks, actionLabel, onAction }) {
     return (
         <section>
-            {/* render tracks here */}
+            {/* Iterate over arrays using JS's .map() */}
+            {tracks.map((track) => (
+                <Track key={track.id} track={track} />
+            ))}
         </section>
     );
 }
