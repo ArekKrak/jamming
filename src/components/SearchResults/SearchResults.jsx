8d33378dc55b878fb308f432b927b08e4416de7e
Arek Krakowiak <369arek12@protonmail.com>
2025-10-21 20:27:23 +0100

    render the TrackList component

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,8 +1,11 @@
-export default function SearchResults({ tracks, onAdd }) {
+import TrackList from "../TrackList/TrackList";
+
+export default function SearchResults({ tracks }) {
     return (
         <section>
             <h3>Search Results</h3>
-            {/* render TrackList here */}
+            {/* Render the TrackList component that displays the actual tracks */}
+            <TrackList tracks={tracks} />
         </section>
     );
 }
