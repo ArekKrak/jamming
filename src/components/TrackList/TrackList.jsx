f23f4deb4931d7b9e1f5d65c16a0100d295fc06e
Arek Krakowiak <369arek12@protonmail.com>
2025-10-25 07:22:31 +0100

    debug no render of the button

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,11 +1,11 @@
 import Track from "../Track/Track"
 
-export default function TrackList({ tracks, onAdd }) {
+export default function TrackList({ tracks, onAdd, onRemove }) {
     return (
         <section>
             {/* Iterate over arrays using JS's .map() */}
             {tracks.map((track) => (
-                <Track key={track.id} track={track} onAdd={onAdd} />
+                <Track key={track.id} track={track} onAdd={onAdd} onRemove={onRemove} />
             ))}
         </section>
     );
