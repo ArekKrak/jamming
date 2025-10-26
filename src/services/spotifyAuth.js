4fb8eaecfaa2b2ba1808429ee52954e83f068883
Arek Krakowiak <369arek12@protonmail.com>
2025-10-26 06:51:29 +0000

    add build-time configuration

--- src/services/spotifyAuth.js
+++ src/services/spotifyAuth.js
@@ -1,3 +1,10 @@
-let accessToken = null;
-let expiryTime = 0;
-let expiryTimer = null;
+let token = null; // current access token (string) or null
+let expireAt = 0; 
+
+// The public identifier obtained from the Spotify Dashboard
+const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+// The exact URL should send the user back to after login
+const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI || window.location.origin + "/" /* This is a fallback, for cases where the env isn't set */;
+// A space-separated list of permissions the requests
+const SCOPES = import.meta.env.VITE_SPOTIFY_SCOPES || "playlist-modify-public";
+
