b85d39b909032e1687646c14b936bdf233637e35
Arek Krakowiak <369arek12@protonmail.com>
2025-10-27 03:28:22 +0000

    import useState and create a state variable for the text

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,9 @@
 import "./SearchBar.css"
+import { useState } from "react"; // enable users to enter a search parameter (we must capture their input first)
 
 export default function SearchBar({ onSearch }) {
+    const [query, setQuery] = useState(""); // enter a search parameter, makes the input a controlled component - UI shows
+                                            // `query`. and `query` updates as the user types
     return (
         <section>
             <input className="input-field" placeholder="Enter a song, artist, or album" />
